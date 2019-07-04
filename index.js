d412665e83ec83ae23b1329589ad045c43ecee5a
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-04 11:09:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 function scuberGreetingForFeet(someValue){
   // Write your code here!
-  let result;
-  if(someValue <= 400) {
-    result = "This one is on me!";
-  }else if (someValue > 2000){
-    result = "I will gladly take your thirty bucks.";
+  let result1 ="This one is on me!";
+  let result2 = "I will gladly take your thirty bucks.";
+  
+  if (someValue <= 400) {
+    return result1;
+  } else if ( someValue > 2000 ) {
+    return result2;
+  } else if ( someValue > 2500 ) {
+    return result2;
+    result2 = "No can do.";
   }
 }
-
 function ternaryCheckCity(){
   // Write your code here!
 }
