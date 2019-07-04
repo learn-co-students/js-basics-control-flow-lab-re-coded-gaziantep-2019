dead9af58c9dbe7e4c386093e97179f20745db22
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-04 07:02:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  let result;
+  if(someValue <= 400) {
+    result = "This one is on me!";
+  }else if (someValue > 2000){
+    result = "I will gladly take your thirty bucks.";
+  }
 }
 
 function ternaryCheckCity(){
