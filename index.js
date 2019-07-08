8978b0d2a4e50143ac2be4103eb8a5c195c4c30a
kassem241 <34236354+kassem241@users.noreply.github.com>
2019-07-08 15:31:58 +0300

    update js

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 2000 && feet < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+    else {
+      return 'No can do.';
+    }
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+   return city == 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+    switch (tip) {
+    case tip = "generous":
+      return "Thank you so much."
+        break;
+    case tip = "not as generous" :
+      return "Thank you."
+        break;
+    default:
+      return "Bye."
+
+  }
 }
