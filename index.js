7b1e5cdc9886340338dc6d7910c70eeaf9c4d9ea
Halil güler <h.guler3717@gmail.com>
2019-07-26 22:03:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  //let feet;
+  if ( feet <= 400) {
+   return "This one is on me!"; 
+  }
+   else if (2000 < feet && 2500 > feet){
+     return "I will gladly take your thirty bucks.";
+   }
+   else {
+     return "No can do.";
+   }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+
+return city === "NYC" ? "Ok, sounds good." : "No go." ;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+ // let tip;
+  switch(tip){
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return 'Thank you.' ;
+    default:
+      return "Bye.";
+  }
 }
